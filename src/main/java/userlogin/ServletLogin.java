0ad651c5f39c14b1ac0be4b694bed7fca13f9e19
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-06-30 12:47:06 +0100

    task - login with parameters and redirect to homepage

--- src/main/java/userlogin/ServletLogin.java
+++ src/main/java/userlogin/ServletLogin.java
@@ -1,8 +1,31 @@
 package userlogin;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/login")
 public class ServletLogin extends HttpServlet {
+
+    String username = "username";
+    String password = "password";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String pwd = req.getParameter("pwd");
+
+        if(name.equals(username) && pwd.equals(password)){
+            resp.sendRedirect(req.getContextPath() + "/home");
+        }else{
+            resp.getWriter().println("Wrong username or password");
+        }
+    }
+
+
+
 }
